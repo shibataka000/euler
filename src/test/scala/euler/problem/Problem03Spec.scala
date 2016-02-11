192a051754b547c4717786116fde477b11c6fa66
shibataka000 <chise.alter.pasta@gmail.com>
2016-02-11 20:07:42 +0900

    Add regression test about problem03.

---
+++ src/test/scala/euler/problem/Problem03Spec.scala
@@ -0,0 +1,11 @@
+package euler.problem
+
+import org.specs2.mutable.Specification
+
+class Problem03Spec extends Specification {
+  "Problem03" should {
+    "return answer 6857" in {
+      Problem03.solve must beEqualTo(6857)
+    }
+  }
+}
