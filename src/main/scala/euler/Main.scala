85b0050a44693e24a18df4581c144319f931bf7b
shibataka000 <chise.alter.pasta@gmail.com>
2016-02-11 13:08:45 +0900

    Solve problem01.

---
+++ src/main/scala/euler/Main.scala
@@ -0,0 +1,9 @@
+package euler
+
+import euler.problem._
+
+object Main {
+  def main(args: Array[String]) = {
+    println(Problem01.solve)
+  }
+}
---
+++ src/main/scala/euler/problem/Problem01.scala
@@ -0,0 +1,5 @@
+package euler.problem
+
+object Problem01 {
+  def solve = (1 to 999).filter(x => x % 3 == 0 || x % 5 == 0).sum
+}
