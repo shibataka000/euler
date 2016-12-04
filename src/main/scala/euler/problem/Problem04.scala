03842d6b29cfd75bdb292b99ae0048bcb4277d2b
shibataka000 <chise.alter.pasta@gmail.com>
2016-12-04 22:05:56 +0900

    Modify problem04 to use BigInt instead of Int.

--- src/main/scala/euler/problem/Problem04.scala
+++ src/main/scala/euler/problem/Problem04.scala
@@ -3,5 +3,5 @@ package euler.problem
 import euler.math.EulerMath
 
 object Problem04 {
-  def solve = (100 to 999).combinations(2).map(_.toList.product).map(x => BigInt(x)).filter(EulerMath.isPalindromicNumber).max
+  def solve = (BigInt(100) to BigInt(999)).combinations(2).map(_.toList.product).filter(EulerMath.isPalindromicNumber).max
 }
